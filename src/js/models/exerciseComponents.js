09632a2a573542d7eaf9bd23a3b6bcc0f432a821
Jean-Christophe Seguin Cabana <jcseguincabana@gmail.com>
2018-07-06 12:54:34 -0400

    updates route adder component

--- src/js/models/exerciseComponents.js
+++ src/js/models/exerciseComponents.js
@@ -4,24 +4,13 @@ import { saveUserData } from './userData';
 const routeAdder = {
   id: 'routeAdder',
   componentNb: 0,
-  UIhtml: (day, exerciseID) => {
-    let html = '';
-    const exerDataArr = day.exercises[exerciseID.split('-')[0]].exerData;
-    if (exerDataArr.length >= 1) {
-      exerDataArr.forEach(el => {
-        html += routeAdder.loggedRoute(el.id, el.compData[0], el.compData[1], el.compData[2], el.compData[3]);
-      });
-    };
-    html += '<button class="routeAdder btn-add">Add route</button>';
-    return html;
-  },
-  loggedRoute: (id, name = "", grade = "", status = "", notes = "") => {return `
+  loggedRoute: id => {return `
     <div id="${id}" class="logged-route">
       <form class="loggedRouteForm">
         <div class="logged-route__inputs">
-          <input type="text" name="route__name" class="route__name" placeholder="Route Name" value="${name}">
-          <input type="text" name="route__grade" class="route__grade" placeholder="Grade" value="${grade}">
-          <select class="route__status" name="route__status" value="${status}">
+          <input type="text" name="route__name" class="route__name" placeholder="Route Name">
+          <input type="text" name="route__grade" class="route__grade" placeholder="Grade">
+          <select class="route__status" name="route__status">
             <option value="Not tried yet">Not tried yet</option>
             <option value="Onsight">Onsight</option>
             <option value="Flash">Flash</option>
@@ -29,7 +18,7 @@ const routeAdder = {
             <option value="Not sent yet">Not sent yet</option>
           </select>
         </div>
-        <textarea class="route__notes" name="route__notes" placeholder="Notes">${notes}</textarea>
+        <textarea class="route__notes" name="route__notes" placeholder="Notes"></textarea>
         <div class="logged-route__btns">
           <button type="submit" class="btn--save">Save modifications</button>
           <button class="btn--save btnDelete">Delete route</button>
@@ -37,6 +26,32 @@ const routeAdder = {
       </form>
     </div>
   `},
+  UIhtml: (day, exerciseID) => {
+    let html = '';
+    const exerDataArr = day.exercises[exerciseID.split('-')[0]].exerData;
+    if (exerDataArr.length >= 1) {
+      exerDataArr.forEach(el => {
+        html += routeAdder.loggedRoute(el.id);
+      });
+    };
+    html += '<button class="routeAdder btn-add">Add route</button>';
+    return html;
+  },
+  mapComponentData: (day, exerciseID) => {
+    const exerDataArr = day.exercises[exerciseID.split('-')[0]].exerData;
+    if (exerDataArr.length >= 1) {
+      exerDataArr.forEach(el => {
+        const route = el.compData[0];
+        const grade = el.compData[1];
+        const status = el.compData[2];
+        const notes = el.compData[3];
+        $(`#${el.id} .route__name`).val(route);
+        $(`#${el.id} .route__grade`).val(grade);
+        $(`#${el.id} .route__status`).val(status);
+        $(`#${el.id} .route__notes`).val(notes);
+      });
+    };
+  },
   setupHandlers: (day, exerciseID) => {
     routeAdder.componentNb = day.exercises[exerciseID.split('-')[0]].exerData.length;
     const exerciseNb = exerciseID.split('-')[0];
@@ -48,6 +63,7 @@ const routeAdder = {
         compData: []
       });
       routeAdder.componentNb++;
+      saveUserData(state);
     });
 
     // 2 - Save to exerData 
@@ -78,8 +94,6 @@ const routeAdder = {
           $(`#${exerciseID} #${componentID}`).remove();
         };
       });
-
-      console.log(day.exercises);
     });
   },
 };
